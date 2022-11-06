d158dc238a6958b09d99ef83e278ea5174727d53
James Kelly <jk377y@gmail.com>
2022-11-06 02:27:28 -0600

    made 4 arrays of characters that could potentially be used

--- assets/script.js
+++ assets/script.js
@@ -29,6 +29,12 @@ function generatePassword() {
   let numberChoice = window.confirm("Would you like NUMBERS ( 0 - 9 ) to be included in your password?\nOK = Yes, Cancel = No");
   let symbolChoice = window.confirm("Would you like SYMBOLS ( !, @, #, $, %, ^, &, *, (, ) ) to be used in your password?\nOK = Yes, Cancel = No");
 
+  // creating arrays of 4 different types with all acceptable characters that may be used for the password
+  let lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
+  let upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  let numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  let symbolArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+
 
 
 //---------------------------------------
